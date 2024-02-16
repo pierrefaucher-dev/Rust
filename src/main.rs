c22e0c1611a3d6319f679bbe0e2e86e1ba762642
Pierre Faucher <contact@pierre-faucher.fr>
2024-02-16 01:10:02 +0100

    Playing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout: std::io::Stdout = stdout();
+    let message: String = String::from("Hello fellow Rustaceans!");
+    let width: usize = message.chars().count();
+
+    let mut writer:BufWriter<std::io::StdoutLock<'_>> = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
